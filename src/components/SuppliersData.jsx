fadd6937e364f13f402c39a337b7ce8b0a2fad01
Carl Manalaysay <carlmanalaysay@Carls-MacBook-Air.local>
2023-10-19 17:09:00 +0800

    added arrow and enter keys functionality on supplier list

--- src/components/SuppliersData.jsx
+++ src/components/SuppliersData.jsx
@@ -1,20 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { AiOutlineRight } from 'react-icons/ai'
 
 function SuppliersData({ filteredData, handleSupplierClick }) {
+  const [selectedItem, setSelectedItem] = useState(-1);
+  console.log(filteredData)
+
+  // Handle arrow key presses to move the selection.
+  const handleKeyDown = (e) => {
+    if (e.key === 'ArrowUp' && selectedItem > 0) {
+      setSelectedItem(selectedItem - 1);
+    } else if (e.key === 'ArrowUp' && selectedItem <= 0) {
+      setSelectedItem(filteredData.length -1)
+    } else if (e.key === 'ArrowDown' && selectedItem < filteredData.length - 1) {
+      setSelectedItem(selectedItem + 1);
+    } else if (e.key === 'ArrowDown' && selectedItem === filteredData.length - 1) {
+      setSelectedItem(0)
+    }
+  };
+
+  // Handle "Enter" key press to trigger handleSupplierClick.
+  const handleEnter = (e) => {
+    if (e.key === 'Enter' && selectedItem >= 0) {
+      handleSupplierClick(filteredData[selectedItem].name)
+    }
+  };
+
+  // Attach event listeners for arrow keys and Enter key.
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [selectedItem]);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleEnter);
+    return () => {
+      document.removeEventListener('keydown', handleEnter);
+    };
+  }, [selectedItem])
+
   return (
     <div className='list-container'>
-        {filteredData.map((data) => (
+      {filteredData.map((data, index) => (
         <div
-            key={data.id}
-            onClick={() => handleSupplierClick(data.name)}
-            className='list-supplier'
+          key={data.id}
+          onClick={() => handleSupplierClick(data.name)}
+          className={`list-supplier ${selectedItem === index ? 'selected' : ''}`}
         >
-            <span>{data.name}</span>
-            <span className='arrow-dropdown'><AiOutlineRight /></span>
+          <span>{data.name}</span>
+          <span className='arrow-dropdown'><AiOutlineRight /></span>
         </div>
-        ))
-        }
+      ))}
   </div>
   )
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { FaMagnifyingGlass } from 'react-icons/fa6'
 import Modal from '../components/Modal'
 
 function Products() {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const modalRef = useRef(null)
+  const browseButtonRef = useRef(null)
+
+  useEffect(() => {
+    if(isModalOpen && modalRef.current) {
+      modalRef.current.focus();
+    }
+  }, [isModalOpen])
+
+  const handleBrowseButtonClick = () => {
+    setIsModalOpen(!isModalOpen);
+      browseButtonRef.current.blur(); // Remove focus from the "Browse" button when closing the modal.
+  };
 
   return (
     <div>
       <div>Product Search</div>
-      <button onClick={() => setIsModalOpen(!isModalOpen)}>Browse</button>
+      <button onClick={handleBrowseButtonClick} ref={browseButtonRef}>Browse</button>
       <div>
         <input
           type='text'
@@ -17,6 +30,7 @@ function Products() {
       </div>
       {isModalOpen && <Modal
         setIsModalOpen={setIsModalOpen}
+        refs={modalRef}
       />}
     </div>
   )
